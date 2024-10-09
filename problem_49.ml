3da671f6c6dc53d50317ca8170bb03b20b520c93
Steve Hussung <steven.hussung@gmail.com>
2024-10-08 20:55:40 -0400

    Beginning problem 49 with list_of_int and frequency counting.

--- problem_49.ml
+++ problem_49.ml
@@ -9,4 +9,27 @@ There are no arithmetic sequences made up of three 1-, 2-, or 3-digit primes, ex
 What 12-digit number do you form by concatenating the three terms in this sequence?
 *)
 
+(* Need to be able to parse through a string. (unsure of how important duplicates are here) *)
 
+(* Lets mimic the Enum.frequencies function from Elixir *)
+
+let rec list_of_int_tr acc n = 
+        match n with
+        | 0 -> acc
+        | n -> list_of_int_tr ((n mod 10)::acc) (n/10)
+;;
+
+let list_of_int n = list_of_int_tr [] n;;
+
+
+let rec count a list =
+        list |>
+        List.filter (fun x -> x = a) |>
+        List.length
+;;
+
+12343434 
+        |> list_of_int 
+        |> count 4
+        |> string_of_int
+        |> print_endline
